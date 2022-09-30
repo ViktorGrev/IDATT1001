de7aea745e5134ec8063b073ae8bf00516c14ee6
Viktor Grevskott <114399917+ViktorGrev@users.noreply.github.com>
2022-09-30 13:34:31 +0200

    Add files via upload

---
+++ øvingsoppgaver/øving1/oppgave1/Oppgave.java
@@ -0,0 +1,10 @@
+/*Oppgave 1
+Skriv et program som regner om fra tommer til centimeter. En tomme er 2,54 centimeter. Sett
+opp testdatasett og prøv ut programmet */
+public class oppgave1 {
+    public static void main(String[] args) {
+      double tomme =  5;
+      double cm = 2.54 * tomme;
+      System.out.println(tomme + " tommer er " + cm + " cm");
+    }
+  }
